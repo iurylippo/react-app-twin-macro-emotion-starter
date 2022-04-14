222881e6e30a381f80c588ed0ab6a2730626db6c
Iury Vieira Lippo da Silva <iuryvieira.lippo@gmail.com>
2022-04-14 17:59:57 -0300

    feat: add tailwind

---
+++ src/App.style.ts
@@ -0,0 +1,17 @@
+import tw from 'twin.macro'
+
+export const AppTitle = tw.h1`
+    text-2xl
+    text-green-500
+`;
+
+export const AppButton = tw.a`
+    rounded 
+    tracking-wide 
+    cursor-pointer 
+    py-1.5 
+    px-4 
+    text-white 
+    font-bold 
+    bg-primary-500
+`
